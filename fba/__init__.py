8d473851ec3294e55c3a95285aa48ae85d6157de
Robert Edwards <raedwards@gmail.com>
2015-11-17 08:02:00 +1000

    PyFBA

--- fba/__init__.py
+++ fba/__init__.py
@@ -1,2 +1,3 @@
 from external_reactions import uptake_and_secretion_reactions, remove_uptake_and_secretion_reactions
 from create_stoichiometric_matrix import create_stoichiometric_matrix
+from bounds import reaction_bounds, compound_bounds
