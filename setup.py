a53ed5c4e637a312ef94dd4e644745fa386f9860
Robert Edwards <raedwards@gmail.com>
2015-11-23 16:21:03 +1000

    PyFBA

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
+import os
 from distutils.core import setup
 from setuptools.command.test import test as TestCommand
+
+import sys
+
 import PyFBA
 import io
 
 # this is taken from https://www.jeffknupp.com/blog/2013/08/16/open-sourcing-a-python-project-the-right-way/ and
 # allows README.txt and CHANGES.txt to automatically be used by PyPI. Create README.txt using http://pandoc.org/:
 # pandoc -t plain -o README.txt README.md
+
+if 'ModelSEEDDatabase' not in os.environ:
+    sys.stderr.write("Please ensure that you install the Model SEED Database somewhere, and set the environment " +
+                     "variable ModelSEEDDatabase to point to that directory.\n" +
+                     " See INSTALLATION.md for more information\n")
+    sys.exit(-1)
+
+if not os.path.exists(os.environ['ModelSEEDDatabase']):
+    sys.stderr.write("The ModelSEEDDatabase environment variable points to {}".format(os.environ['ModelSEEDDatabase']) +
+                     " but that location does not exist\n")
+    sys.exit(-1)
+
+if not os.path.exists(os.path.join(os.environ['ModelSEEDDatabase'], 'Biochemistry/reactions.master.tsv')):
+    sys.stderr.write("The ModelSEEDDatabase at {} ".format(os.environ['ModelSEEDDatabase']) +
+                     "does not seem to be complete. Did you check it out from GitHub?\n")
+    sys.exit(-1)
+
 
 def read(*filenames, **kwargs):
     encoding = kwargs.get('encoding', 'utf-8')
@@ -47,6 +68,8 @@ setup(
     description='A Python implementation of flux balance analysis',
     tests_require = ['nose'],
     cmdclass={'test': NoseTestCommand},
+    install_requires=['beautifulsoup4', 'glpk=>0.3.1'],
+    dependency_links = ['https://github.com/bradfordboyle/pyglpk/tarball/master#egg=glpk-0.3.1'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment:: Console',
