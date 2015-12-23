0470ea02b11c9151ac49ae21e5233b0e07e27d04
Robert Edwards <raedwards@gmail.com>
2015-12-23 11:52:07 +1000

    PyFBA

--- PyFBA/fba/__init__.py
+++ PyFBA/fba/__init__.py
@@ -2,6 +2,7 @@ from external_reactions import uptake_and_secretion_reactions, remove_uptake_and
 from create_stoichiometric_matrix import create_stoichiometric_matrix
 from bounds import reaction_bounds, compound_bounds
 from run_fba import run_fba
+from fluxes import reaction_fluxes
 
 __all__ = ['uptake_and_secretion_reactions', 'remove_uptake_and_secretion_reactions', 'create_stoichiometric_matrix',
-           'reaction_bounds', 'compound_bounds', 'run_fba']
+           'reaction_bounds', 'compound_bounds', 'run_fba', 'reaction_fluxes']
