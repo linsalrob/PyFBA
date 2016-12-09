5d8d444881473a90de2693fb67f4777a7c530985
dacuevas <dcuevas08@gmail.com>
2016-12-08 21:40:59 -0800

    Fix on function names in __init__.py file.

--- PyFBA/model/__init__.py
+++ PyFBA/model/__init__.py
@@ -1,8 +1,8 @@
 
 from model import Model
 from build_model import roles_to_model
-from fba import model_reaction_fluxes, stdout_fba, stdout_model
+from fba import model_reaction_fluxesa, output_fba
 
 __all__ = ["Model",
            "roles_to_model",
-           "model_reaction_fluxes", "stdout_fba", "stdout_model"]
+           "model_reaction_fluxes", "output_fba"]
