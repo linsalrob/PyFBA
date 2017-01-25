687ca55dd3f02a5bb1a8b60ab47cddd6c1b234f7
Robert Edwards <raedwards@gmail.com>
2017-01-25 10:21:54 -0800

    updating to python3

--- PyFBA/fba/__init__.py
+++ PyFBA/fba/__init__.py
@@ -1,8 +1,8 @@
-from external_reactions import uptake_and_secretion_reactions, remove_uptake_and_secretion_reactions
-from create_stoichiometric_matrix import create_stoichiometric_matrix
-from bounds import reaction_bounds, compound_bounds
-from run_fba import run_fba
-from fluxes import reaction_fluxes
+from .external_reactions import uptake_and_secretion_reactions, remove_uptake_and_secretion_reactions
+from .create_stoichiometric_matrix import create_stoichiometric_matrix
+from .bounds import reaction_bounds, compound_bounds
+from .run_fba import run_fba
+from .fluxes import reaction_fluxes
 
 __all__ = ['uptake_and_secretion_reactions', 'remove_uptake_and_secretion_reactions', 'create_stoichiometric_matrix',
            'reaction_bounds', 'compound_bounds', 'run_fba', 'reaction_fluxes']
--- PyFBA/model/__init__.py
+++ PyFBA/model/__init__.py
@@ -1,7 +1,7 @@
 
-from model import Model
-from build_model import roles_to_model, save_model, load_model
-from fba import model_reaction_fluxes, output_fba, output_fba_with_subsystem
+from .model import Model
+from .build_model import roles_to_model, save_model, load_model
+from .fba import model_reaction_fluxes, output_fba, output_fba_with_subsystem
 
 __all__ = ["Model",
            "roles_to_model", "save_model", "load_model",
