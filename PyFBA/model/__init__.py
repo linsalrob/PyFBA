92b067d785ddc3117c585ddd5d3f457db62c1e15
dacuevas <dcuevas08@gmail.com>
2017-03-22 18:24:00 -0700

    Intial commit of network.py.

--- PyFBA/model/__init__.py
+++ PyFBA/model/__init__.py
@@ -2,7 +2,9 @@
 from .model import Model
 from .build_model import roles_to_model, save_model, load_model
 from .fba import model_reaction_fluxes, output_fba, output_fba_with_subsystem
+from .network import rxn_to_rxn_network, number_of_nodes
 
 __all__ = ["Model",
            "roles_to_model", "save_model", "load_model",
-           "model_reaction_fluxes", "output_fba", "output_fba_with_subsystem"]
+           "model_reaction_fluxes", "output_fba", "output_fba_with_subsystem",
+           "rxn_to_rxn_network", "number_of_nodes"]
