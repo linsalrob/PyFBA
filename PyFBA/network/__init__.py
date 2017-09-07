416c30951913bec2caf4a86d0a7ce6dfbaeb01f6
dacuevas <dcuevas08@gmail.com>
2017-09-07 12:40:14 -0700

    Move SIF file output to its own script

--- PyFBA/network/__init__.py
+++ PyFBA/network/__init__.py
@@ -1,10 +1,12 @@
-from .network import Network, network_compounds_sif, network_reactions_sif
+from .network import Network
+from .files import network_compounds_sif, network_reactions_sif
 from .metrics import clustering_coeff, avg_clustering_coeff, \
     closeness_centrality, avg_closeness_centrality, \
     betweenness_centrality, avg_betweenness_centrality, \
     diameter, number_connected_components, jaccard_distance
 
-__all__ = ["Network", "network_sif",
+__all__ = ["Network",
+           "network_compounds_sif", "network_reactions_sif",
            "clustering_coeff", "avg_clustering_coeff",
            "closeness_centrality", "avg_closeness_centrality",
            "betweenness_centrality", "avg_betweenness_centrality",
