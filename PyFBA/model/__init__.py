87253fb042e7e78fff69d37b7ac65c5364233813
dacuevas <dcuevas08@gmail.com>
2017-10-02 17:52:24 -0700

    Add save_sbml() function in namespace

--- PyFBA/model/__init__.py
+++ PyFBA/model/__init__.py
@@ -1,9 +1,9 @@
 
 from .model import Model
-from .build_model import roles_to_model, save_model, load_model
+from .build_model import roles_to_model, save_model, load_model, save_sbml
 from .fba import model_reaction_fluxes, output_fba, output_fba_with_subsystem
 
 __all__ = ["Model",
-           "roles_to_model", "save_model", "load_model",
+           "roles_to_model", "save_model", "load_model", "save_sbml",
            "model_reaction_fluxes", "output_fba", "output_fba_with_subsystem",
            "rxn_to_rxn_network", "number_of_nodes"]
