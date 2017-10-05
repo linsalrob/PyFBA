53bc9ecf33dc3697e71179461e382b48b0660668
dacuevas <dcuevas08@gmail.com>
2017-10-05 10:00:53 -0700

    Include load_sbml() function into __init__.py

--- PyFBA/model/__init__.py
+++ PyFBA/model/__init__.py
@@ -1,9 +1,11 @@
 
 from .model import Model
-from .build_model import roles_to_model, save_model, load_model, save_sbml
+from .build_model import roles_to_model, save_model, load_model, save_sbml,\
+    load_sbml
 from .fba import model_reaction_fluxes, output_fba, output_fba_with_subsystem
 
 __all__ = ["Model",
            "roles_to_model", "save_model", "load_model", "save_sbml",
+           "load_sbml",
            "model_reaction_fluxes", "output_fba", "output_fba_with_subsystem",
            "rxn_to_rxn_network", "number_of_nodes"]
