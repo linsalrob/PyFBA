1475a0a5a1bc028cf47472b53d29a4ec253ada44
dacuevas <dcuevas08@gmail.com>
2017-12-01 10:14:04 -0800

    New function to calculate compound counts
    compute_compound_counts() uses reaction fluxes from FBA to calculate
    usage or production of compounds

--- PyFBA/model/fba.py
+++ PyFBA/model/fba.py
@@ -9,11 +9,11 @@ def model_reaction_fluxes(model, media_file, biomass_reaction=None):
     Run FBA on model and return dictionary of reaction ID and flux.
 
     :param model: Model object to obtain fluxes from
-    :type model: Model
+    :type model: PyFBA.model.Model
     :param media_file: Media filepath
     :type media_file: str
     :param biomass_reaction: Given biomass Reaction object
-    :type biomass_reaction: Reaction
+    :type biomass_reaction: PyFBA.metabolism.Reaction
     :rtype: dict
     """
     status, value, growth = model.run_fba(media_file, biomass_reaction)
@@ -29,11 +29,11 @@ def output_fba(f, model, media_file, biomass_reaction=None):
     :param f: File object to print to
     :type f: file
     :param model: Model object to obtain fluxes from
-    :type model: Model
+    :type model: PyFBA.model.Model
     :param media_file: Media filepath
     :type media_file: str
     :param biomass_reaction: Given biomass Reaction object
-    :type biomass_reaction: Reaction
+    :type biomass_reaction: PyFBA.metabolism.Reaction
     """
     # Get mapping from reaction IDs to roles
     mReactions = {r: [] for r in model.reactions.keys()}
@@ -62,11 +62,11 @@ def output_fba_with_subsystem(f, model, media_file, biomass_reaction=None):
     :param f: File object to print to
     :type f: file
     :param model: Model object to obtain fluxes from
-    :type model: Model
+    :type model: PyFBA.model.Model
     :param media_file: Media filepath
     :type media_file: str
     :param biomass_reaction: Given biomass Reaction object
-    :type biomass_reaction: Reaction
+    :type biomass_reaction: PyFBA.metabolism.Reaction
     """
     # Get mapping from reaction IDs to roles
     mReactions = {r: [] for r in model.reactions.keys()}
@@ -111,3 +111,105 @@ def output_fba_with_subsystem(f, model, media_file, biomass_reaction=None):
             role = ss = subcat = cat = "Unknown"
             f.write("\t".join([r, str(flux), role, ss, subcat, cat]))
             f.write("\n")
+
+
+def compute_compound_counts(model, media_file, biomass_reaction=None,
+                            verbose=False):
+    """
+    Using FBA, calculate overall compound counts after summing reaction fluxes
+
+    :param model: Model object
+    :type model: PyFBA.model.Model
+    :param media_file: Media filepath
+    :type media_file: str
+    :param biomass_reaction: Given biomass Reaction object
+    :type biomass_reaction: PyFBA.metabolism.Reaction
+    :return: Compound counts
+    :rtype: dict
+    """
+    # Run FBA and obtain reaction fluxes
+    fluxes = model_reaction_fluxes(model, media_file, biomass_reaction)
+    if verbose:
+        print("FBA completed", file=sys.stderr)
+
+    # Load ModelSEED database
+    compounds, reactions, enzymes = \
+        PyFBA.parse.compounds_reactions_enzymes(model.organism_type)
+
+    cpd_counts = {"e": {}, "c": {}}
+    # Iterate through fluxes to calculate compound counts
+    for rxnID, rflux in fluxes.items():
+        # First check for biomass reactions
+        if rxnID == "BIOMASS_EQN":
+            cpd_counts["c"]["Biomass"] = rflux
+            continue
+
+        # Biomass has its own secretion reaction, we will skip it
+        elif rxnID == "UPTAKE_SECRETION_REACTION cpd11416":
+            continue
+
+        # Uptake and secretion reactions will be included here but not
+        # present in the ModelSEED database
+        # The compound ModelSEED ID is located at the end of the name
+        elif rxnID.startswith("UPTAKE_SECRETION_REACTION"):
+            cpdID = rxnID.replace("UPTAKE_SECRETION_REACTION ", "")
+
+            # Find the compound in the model
+            cpd = None
+            for c in model.compounds:
+                if c.model_seed_id == cpdID and c.location == "e":
+                    cpd = c
+                    break
+
+            # Check if compound was found
+            if cpd is None and rflux != 0 and verbose:
+                print("Compound {} was not found in model but was found in an "
+                      "uptake and secretion reaction with flux".format(cpdID),
+                      file=sys.stderr)
+                print("Reaction: {}\tFlux:{}".format(rxnID, rflux),
+                      file=sys.stderr)
+                continue
+
+            # Add flux to the cpd_count
+            cpd_name = str(cpd)
+            if cpd_name not in cpd_counts["e"]:
+                cpd_counts["e"][cpd_name] = 0
+
+            # The right side of these reactions are always the boundary
+            # sink and the left side are always the external compound
+            # Negate flux since a negative flux of this reaction means
+            # the compound is being produced and not consumed
+            cpd_counts["e"][cpd_name] += -rflux
+            continue
+
+        # Get left compounds
+        for cpd in reactions[rxnID].left_compounds:
+            loc = cpd.location
+            cpd_name = str(cpd)
+            if cpd_name not in cpd_counts[loc]:
+                cpd_counts[loc][cpd_name] = 0
+
+            # Add flux to cpd_count
+            # Check direction because a leftward reaction means left
+            # compounds are products and not reactants
+            if reactions[rxnID].direction == "<":
+                cpd_counts[loc][cpd_name] += rflux
+            else:
+                cpd_counts[loc][cpd_name] += -rflux
+
+        # Get right compounds
+        for cpd in reactions[rxnID].right_compounds:
+            cpd_name = str(cpd)
+            loc = cpd.location
+            if cpd_name not in cpd_counts[loc]:
+                cpd_counts[loc][cpd_name] = 0
+
+            # Add flux to cpd_count
+            # Check direction because a leftward reaction means right
+            # compounds are reactants and not products
+            if reactions[rxnID].direction == "<":
+                cpd_counts[loc][cpd_name] += -rflux
+            else:
+                cpd_counts[loc][cpd_name] += rflux
+
+    return cpd_counts
