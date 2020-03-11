aec15d971b15bf808d1592f08be048020166f8d1
Shane Levi <shanealevi@gmail.com>
2020-03-10 18:25:14 -0700

    made extract_inchi.py compatible with reaction files

--- example_code/extract_inchi.py
+++ example_code/extract_inchi.py
@@ -1,6 +1,6 @@
 """
 This code is designed to extract InCHI keys for the relevant compounds in a metabolic model to be used in
-metabolomic analysis. Currently only usable with the ModelSEED Database.
+metabolomic analysis.
 """
 
 import argparse
@@ -15,77 +15,100 @@ MODELSEED_DIR = ""
 if 'ModelSEEDDatabase' in os.environ:
         MODELSEED_DIR = os.environ['ModelSEEDDatabase']
 else:
-    sys.stderr.write(f"{bcolors.FAIL}FATAL: {bcolors.ENDC}")
     sys.stderr.write("Please ensure that you install the Model SEED Database somewhere, and set the environment " +
                      "variable ModelSEEDDatabase to point to that directory.\n" +
                      " See INSTALLATION.md for more information\n")
     sys.exit(-1)
 
 if not MODELSEED_DIR:
-    sys.stderr.write(f"{bcolors.FAIL}FATAL: {bcolors.ENDC}")
     sys.stderr.write("The ModelSEEDDatabase environment variable is not set.\n")
     sys.stderr.write("Please install the ModelSEEDDatabase, set the variable, and try again")
     sys.exit(-1)
 
 if not os.path.exists(MODELSEED_DIR):
-    sys.stderr.write(f"{bcolors.FAIL}FATAL: {bcolors.ENDC}")
     sys.stderr.write("The MODEL SEED directory: {} does not exist.\n".format(MODELSEED_DIR))
     sys.stderr.write("Please check your installation.\n")
     sys.exit(-1)
 
-parser = argparse.ArgumentParser(description="Create a list of InCHI keys from compounds in an sbml file")
-parser.add_argument('-i', help='sbml input file', required=True)
+parser = argparse.ArgumentParser(description="Create a list of InCHI keys from compounds in an sbml/reaction file")
+parser.add_argument('-s', help='sbml input file')
+parser.add_argument('-r', help='reactions input file')
 parser.add_argument('-o', help='output file')
 parser.add_argument('-v', help='verbose output', action='store_true')
 args = parser.parse_args()
 
-if not os.path.exists(args.i):
-    sys.exit("SBML file {} was not found".format(args.i))
+# Checking that an input is specified
+if not args.s and not args.r:
+    sys.exit("Either -s (sbml file) or -r (reactions file) must be specified.")
 
-out = "{}.inchi.tsv".format(args.i[:-5])
-if args.o:
-    out = args.o
+# Checking for double input
+if args.s and args.r:
+    sys.exit("Only one input type at a time please.")
 
-sbml = PyFBA.parse.parse_sbml_file(args.i, False)
-cids = {}
+# Mapping reactions, compounds, and InChI keys
+cnames = {}
 ckeys = {}
+rcpds = {}
 
-compounds_file = os.path.join(MODELSEED_DIR, 'Biochemistry/compounds.json')
-
-# creating a dict of each compound ID to its InCHI key
 try:
-    with open(compounds_file, 'r') as f:
-        mseed = json.load(f)
+    with open(os.path.join(MODELSEED_DIR, 'Biochemistry/compounds.json'), 'r') as cfile:
+        compounds = json.load(cfile)
+        for cpd in compounds:
+            cnames[cpd] = compounds[cpd]['name']
+            ckeys[cpd] = compounds[cpd]['inchikey']
 
-    for cpd in mseed:
-        key = mseed[cpd]['inchikey']
-        ckeys[cpd] = key
+    with open(os.path.join(MODELSEED_DIR, 'Biochemistry/reactions.json'), 'r') as rfile:
+        reactions = json.load(rfile)
+        for rxn in reactions:
+            cpds = reactions[rxn]['compound_ids']
+            cpds = cpds.strip().split(';')
+            rcpds[rxn] = cpds
+
 except:
-    print('Could not read compounds.json; please set the MODELSEED_DIR path variable.')
+    print('Failed to parse the ModelSEED Biochemistry Database.')
 
-# get the compounds from the SBML file. This is a dict of metabolite.Compound objects
-cpdids = sbml.compounds_by_id
-if args.v:
-    sys.stderr.write("List of 'cpds': {}\n".format(len(cpds)))
-
-# creating a dict of each compound in the sbml matching its name to its id
-for cpd_id, cpd_obj in cpdids.items():
+# Attempt to parse an SBML
+if args.s:
     try:
-        newcm, cpdname, cpdloc = cpd_id.split("_")
+        sbml = PyFBA.parse.parse_sbml_file(args.s, False)
+
+        if args.o:
+            out = args.o
+        else:
+            out = "{}.inchi.txt".format(args.s)
+
+        with open(out, 'w') as outfile:
+            for cpd in sbml.compounds_by_id:
+                parts = cpd.split('_')
+                c = parts[0]
+                name = cnames.get(c, "none")
+                key = ckeys.get(c, "none")
+                outfile.write(f"{c}\t{name}\t{key}\n")
+
     except:
-        cpdname, cpdloc = cpd_id.split("_")
-    cpd_name = cpd_obj.name
-    cids[cpdname] = cpd_name
+            sys.exit("There was an error parsing the SBML.")
 
-with open(out, 'w') as outfile:
-    for id, name in cids.items():
-        inkey = ckeys[id]
-        try:
-            if inkey:
-                outfile.write("{}\t{}\t{}\n".format(id, name, inkey))
-            else:
-                outfile.write("{}\t{}\tnone\n".format(id, name))
-        except:
-            print("{}: {} has no InCHI key".format(id, name))
+# Attempt to parse a reactions file
+if args.r:
+    try:
+        cpds = set()
+        with open(args.r, 'r') as infile:
+            for line in infile:
+                rxn = line.strip()
+                rxn_cpds = rcpds.get(rxn, '')
+                for c in rxn_cpds:
+                    cpds.add(c)
 
+        if args.o:
+            out = args.o
+        else:
+            out = "{}.inchi.txt".format(args.r)
 
+        with open(out, 'w') as outfile:
+            for cpd in cpds:
+                name = cnames.get(cpd, "none")
+                key = ckeys.get(cpd, "none")
+                outfile.write(f"{cpd}\t{name}\t{key}\n")
+
+    except:
+            sys.exit("There was an error parsing the reactions file.")
