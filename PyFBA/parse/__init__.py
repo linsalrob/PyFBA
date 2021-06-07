89997b253a9476299010281ac4e5d7550a1049fd
linsalrob <raedwards@gmail.com>
2021-06-07 11:45:05 +0930

    adding parse_model_seed_data

--- PyFBA/parse/__init__.py
+++ PyFBA/parse/__init__.py
@@ -1,5 +1,5 @@
 from .read_media import read_media_file
 from .rast import read_assigned_functions, roles_of_function, roles_to_subsystem
-from .model_seed import compounds_reactions_enzymes
+from .model_seed import compounds_reactions_enzymes, parse_model_seed_data
 from .SBML import parse_sbml_file, correct_media_names
 
