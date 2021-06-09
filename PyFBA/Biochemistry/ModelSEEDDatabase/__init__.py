80c4568cb1b8de6af4f242c13ceac71c3427c2e9
linsalrob <raedwards@gmail.com>
2021-06-09 15:08:01 +0930

    inserting a note in __init__.py

--- PyFBA/Biochemistry/ModelSEEDDatabase/__init__.py
+++ PyFBA/Biochemistry/ModelSEEDDatabase/__init__.py
@@ -1,4 +1,8 @@
 """
-Biochemistry is now a module so we can import it and use importlib.resources to access the data
+This file is here to convert this directory into a module, so that the
+directory is visible as a package resource.
+
+If you update the data in this directory, please retain this __init__.py
+or create a new, empty file with that name!
 """
 
