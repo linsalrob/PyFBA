3b1a7012e3788559e7ab93f5a2a82f44b4af3db1
Rob Edwards <raedwards@gmail.com>
2021-06-09 14:51:21 +0930

    Removing ModelSEEDDatabase connections

--- PyFBA/tests/test_model_seed_parsing.py
+++ PyFBA/tests/test_model_seed_parsing.py
@@ -4,26 +4,6 @@ import unittest
 
 import PyFBA
 
-MODELSEED_DIR = ""
-if os.path.exists('Biochemistry/ModelSEEDDatabase'):
-    MODELSEED_DIR = 'Biochemistry/ModelSEEDDatabase'
-elif 'ModelSEEDDatabase' in os.environ:
-        MODELSEED_DIR = os.environ['ModelSEEDDatabase']
-else:
-    sys.stderr.write("Please ensure that you install the Model SEED Database somewhere, and set the environment " +
-                     "variable ModelSEEDDatabase to point to that directory.\n" +
-                     " See INSTALLATION.md for more information\n")
-    sys.exit(-1)
-
-if not MODELSEED_DIR:
-    sys.stderr.write("The ModelSEEDDatabase environment variable is not set.\n")
-    sys.stderr.write("Please install the ModelSEEDDatabase, set the variable, and try again")
-    sys.exit(-1)
-
-if not os.path.exists(MODELSEED_DIR):
-    sys.stderr.write("The MODEL SEED directory: {} does not exist.\n".format(MODELSEED_DIR))
-    sys.stderr.write("Please check your installation.\n")
-    sys.exit(-1)
 
 class TestModelSeedParsing(unittest.TestCase):
 
@@ -31,7 +11,7 @@ class TestModelSeedParsing(unittest.TestCase):
         """
         This is run before everything else
         """
-        self.assertTrue(os.path.exists(MODELSEED_DIR))
+
 
     def test_compounds(self):
         """
