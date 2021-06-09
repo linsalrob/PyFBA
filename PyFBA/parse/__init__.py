42e935ea6a7f7b01894cebf62d9445d1ed7a90e3
Rob Edwards <raedwards@gmail.com>
2021-06-09 15:52:10 +0930

    Adding a local media option

--- PyFBA/parse/__init__.py
+++ PyFBA/parse/__init__.py
@@ -1,4 +1,4 @@
-from .read_media import read_media_file
+from .read_media import read_media_file, pyfba_media, media_files
 from .rast import read_assigned_functions, roles_of_function, roles_to_subsystem
 from .model_seed import compounds_reactions_enzymes, parse_model_seed_data
 from .SBML import parse_sbml_file, correct_media_names
--- PyFBA/parse/read_media.py
+++ PyFBA/parse/read_media.py
@@ -1,7 +1,66 @@
 import os
 import sys
 
+from PyFBA import log_and_message
+
+try:
+    from importlib.resources import open_text, path
+except ImportError:
+    # this is for python<3.7
+    from importlib_resources import open_text, path
+
 import PyFBA
+
+def media_files():
+    """
+    Get a list of the media names that we know about
+    :return: a dict of the media names and the data repositories associated wtih those names
+    """
+
+    all_media = {}
+    with path('PyFBA.Biochemistry', 'media') as media_path:
+        for m in os.listdir(media_path):
+            if 'README.md' == m or '__init__.py' == m:
+                continue
+            n = m.replace('.txt', '')
+            all_media[n] = m
+    return all_media
+
+
+def pyfba_media(media_name):
+    """
+    Parse a media file that we have provided with PyFBA. This is probably the most common method to access media.
+    If you want alternative media included, just let us know!
+
+    Returns a set of compounds that are in the media.
+
+    :param mediaf: The name of the media to read
+    :type mediaf: str
+    :return: A set of media components
+    :rtype: set of metabolism.Compound
+    """
+
+    all_media = media_files()
+    if media_name not in media_files():
+        am = "\n".join(map(str, all_media.keys()))
+        log_and_message(f"Sorry, we don't know what media {media_name} is. It should be one of {am}", stderr=True)
+        return set()
+
+    media = set()
+
+    with open_text("PyFBA.Biochemistry.media", all_media[media_name]) as f:
+        for li, l in enumerate(f):
+            # skip the header line
+            if li == 0:
+                continue
+            p = l.strip().split("\t")
+            if len(p) < 2:
+                sys.stderr.write("Skipped line {} as it does not have enough columns\n".format(l.strip()))
+                continue
+            c = PyFBA.metabolism.CompoundWithLocation(f"Media{li:03}", p[1], 'e')
+            media.add(c)
+
+    return media
 
 
 def read_media_file(mediaf):
@@ -39,5 +98,3 @@ def read_media_file(mediaf):
             media.add(c)
     
     return media
-
-
