0d8d8a7761db6522825fb284e0d1696256561b4f
Rob Edwards <raedwards@gmail.com>
2021-06-15 23:44:58 +0930

    Debugging (probably)

--- PyFBA/fba/bounds.py
+++ PyFBA/fba/bounds.py
@@ -62,8 +62,8 @@ def reaction_bounds(reactions, reactions_with_upsr, media, lower=-1000.0, mid=0.
 
             if in_media:
                 # This is what I think it should be:
-                #rbvals[r] = (lower, upper)
-                rbvals[r] = (0.0, upper)
+                rbvals[r] = (lower, upper)
+                #rbvals[r] = (0.0, upper)
                 log_and_message(f"{r} {reactions[r].equation}  ({rbvals[r]})", stderr=verbose)
                 media_uptake_secretion_count += 1
             else:
@@ -82,8 +82,8 @@ def reaction_bounds(reactions, reactions_with_upsr, media, lower=-1000.0, mid=0.
             # rbvals[r] =  (lower, upper)
         elif direction == "<":
             # This is what I think it should be:
-            rbvals[r] = (lower, mid)
             # rbvals[r] = (lower, mid)
+            rbvals[r] = (lower, upper)
         else:
             sys.stderr.write("DO NOT UNDERSTAND DIRECTION " + direction + " for " + r + "\n")
             rbvals[r] = (mid, upper)
