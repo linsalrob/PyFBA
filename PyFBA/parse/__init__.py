f7785bcc8bbe39d03bce22638ae69768ba30ec15
Rob Edwards <raedwards@gmail.com>
2021-06-17 15:39:44 +0930

    Refactoring correct names into the media file. Makes more sense.

--- PyFBA/parse/__init__.py
+++ PyFBA/parse/__init__.py
@@ -1,5 +1,5 @@
-from .read_media import read_media_file, pyfba_media, media_files
-from .rast import read_assigned_functions, roles_of_function, roles_to_subsystem
+from .read_media import read_media_file, pyfba_media, media_files, correct_media_names, normalized_media
+from .rast import read_assigned_functions, roles_of_function, roles_to_subsystem, read_functional_roles
 from .model_seed import compounds_reactions_enzymes, parse_model_seed_data
-from .SBML import parse_sbml_file, correct_media_names
+from .SBML import parse_sbml_file
 
