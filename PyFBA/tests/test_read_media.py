ea4d72df3b34ceb21c438280942070f0ff2a5a34
Rob Edwards <raedwards@gmail.com>
2021-06-17 16:55:42 +0930

    Media handled internally now

--- PyFBA/tests/test_read_media.py
+++ PyFBA/tests/test_read_media.py
@@ -5,27 +5,11 @@ import sys
 
 import PyFBA
 
-media_file_loc = ''
-if os.path.exists('../media/ArgonneLB.txt'):
-    media_file_loc = '../media'
-elif os.path.exists('media/ArgonneLB.txt'):
-    media_file_loc = 'media'
-elif 'PYFBA_MEDIA_DIR' in os.environ and os.path.exists(os.path.join(os.environ['PYFBA_MEDIA_DIR'], 'ArgonneLB.txt')):
-    media_file_loc = os.environ['PYFBA_MEDIA_DIR']
-else:
-    sys.stderr.write("No media found. Can't proceed with the tests.\n")
-    sys.stderr.write("You can specify the media location by setting the PYFBA_MEDIA_DIR environment variable\n")
-
-
 class TestReadMedia(unittest.TestCase):
-    def setUp(self):
-        self.assertTrue(os.path.exists(os.path.join(media_file_loc, 'ArgonneLB.txt')))
 
     def test_read_media_file(self):
         """Test reading a media file"""
-        if media_file_loc == "":
-            return
-        media = PyFBA.parse.read_media_file(os.path.join(media_file_loc, 'ArgonneLB.txt'))
+        media = PyFBA.parse.raw_media('ArgonneLB')
         self.assertEqual(len(media), 65)
         testm = None
         for m in  media:
