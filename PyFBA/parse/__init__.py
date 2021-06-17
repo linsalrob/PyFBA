02cfdcdabe8dc73ace2867a2dbda1b93c66d004b
Rob Edwards <raedwards@gmail.com>
2021-06-17 15:58:42 +0930

    Correcting media reading name

--- PyFBA/parse/__init__.py
+++ PyFBA/parse/__init__.py
@@ -1,4 +1,4 @@
-from .read_media import read_media_file, pyfba_media, media_files, correct_media_names, normalized_media
+from .read_media import read_media_file, pyfba_media, media_files, correct_media_names, raw_media
 from .rast import read_assigned_functions, roles_of_function, roles_to_subsystem, read_functional_roles
 from .model_seed import compounds_reactions_enzymes, parse_model_seed_data
 from .SBML import parse_sbml_file
