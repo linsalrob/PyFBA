4e09bb25ab6d0d11465896fb373626d7a0aad0d9
Rob Edwards <raedwards@gmail.com>
2021-06-19 13:34:03 +0930

    Adding imports to __init__.py

--- PyFBA/cmd/__init__.py
+++ PyFBA/cmd/__init__.py
@@ -5,6 +5,8 @@ from .assigned_functions_to_reactions import to_reactions
 from .fba_from_reactions import run_the_fba
 from .gapfill_from_reactions_multiple_conditions import gapfill_multiple_media
 
+# Don't forget to add the imports here so that you can import *
+
 __all__ = [
     'cite_me_please', 'measure_fluxes', 'gapfill_from_roles', 'to_reactions', 'run_the_fba', 'gapfill_multiple_media'
 ]
