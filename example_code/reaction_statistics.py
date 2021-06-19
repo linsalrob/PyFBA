2baa7b0a99e725b14d652e7a71caee66b3c56e6f
Rob Edwards <raedwards@gmail.com>
2021-06-19 17:39:40 +0930

    Merged stats into one file

--- example_code/reaction_statistics.py
+++ example_code/reaction_statistics.py
@@ -3,41 +3,34 @@ Print statistics about the reactions. Note that this is used in test_model_seed_
 can run this code to update those numbers!
 """
 
-import os
-import sys
 import PyFBA
+from timeit import default_timer as timer
 
-import PyFBA
-MODELSEED_DIR = ""
-if 'ModelSEEDDatabase' in os.environ:
-        MODELSEED_DIR = os.environ['ModelSEEDDatabase']
-else:
-    sys.stderr.write("Please ensure that you install the Model SEED Database somewhere, and set the environment " +
-                     "variable ModelSEEDDatabase to point to that directory.\n" +
-                     " See INSTALLATION.md for more information\n")
-    sys.exit(-1)
-if not MODELSEED_DIR:
-    sys.stderr.write("The ModelSEEDDatabase environment variable is not set.\n")
-    sys.stderr.write("Please install the ModelSEEDDatabase, set the variable, and try again")
-    sys.exit(-1)
-if not os.path.exists(MODELSEED_DIR):
-    sys.stderr.write("The MODEL SEED directory: {} does not exist.\n".format(MODELSEED_DIR))
-    sys.stderr.write("Please check your installation.\n")
-    sys.exit(-1)
+start = timer()
+modeldata = PyFBA.parse.model_seed.parse_model_seed_data("gramnegative", verbose=False)
+end = timer()
 
-
-compounds, reactions = PyFBA.parse.model_seed.reactions()
-
-print(f"self.assertGreaterEqual(len(compounds), {len(compounds)})")
-print(f"self.assertGreaterEqual(len(reactions), {len(reactions)})")
+print(f"self.assertGreaterEqual(len(compounds), {len(modeldata.compounds)})")
+print(f"self.assertGreaterEqual(len(reactions), {len(modeldata.reactions)})")
 is_transport = 0
 direction = {}
-for r in reactions:
-    if reactions[r].is_transport:
+for r in modeldata.reactions:
+    if modeldata.reactions[r].is_transport:
         is_transport += 1
-    direction[reactions[r].direction] = direction.get(reactions[r].direction, 0) + 1
+    direction[modeldata.reactions[r].direction] = direction.get(modeldata.reactions[r].direction, 0) + 1
 
 
 print(f"self.assertGreaterEqual(is_transport, {is_transport})")
 for k in (direction):
     print(f"self.assertGreaterEqual(direction['{k}'], {direction[k]})")
+
+
+print(f"Initially there are {len(modeldata.compounds)} compounds.")
+print(f"Initially there are {len(modeldata.reactions)} reactions.")
+print(f"Initially there are {len(modeldata.enzymes)} enzymes.")
+print(f"This search took {end-start} seconds")
+reactions = PyFBA.parse.model_seed.reactions()
+compounds = PyFBA.parse.model_seed.compounds()
+
+PyFBA.parse.model_seed.reset_cache()
+
