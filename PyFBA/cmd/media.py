6c4d675099a22bf59c75addde1808bb186a16b03
linsalrob <raedwards@gmail.com>
2021-10-09 09:12:21 +1030

    Add a function to list compounds in media

--- PyFBA/cmd/media.py
+++ PyFBA/cmd/media.py
@@ -12,3 +12,17 @@ def list_media():
         print(m)
 
 
+def compounds_in_media(m, model_data=None, verbose=False):
+    """
+    Print the compounds in the media
+    :param m: the media
+    :param model_data: a model data object (not necessary)
+    :param verbose: more output
+    """
+
+    if not model_data:
+        model_data = PyFBA.parse.model_seed.parse_model_seed_data(verbose=verbose)
+
+    media = PyFBA.parse.read_media.find_media_file(m, model_data, verbose)
+    for c in media:
+        print(c)
